507eaba9db1141d793e29da88bf43ba7279635cd
rosekairu <rosekairu@gmail.com>
2023-06-16 18:47:44 +0300

    modified settings

--- Blog/settings.py
+++ Blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+import cloudinary
 from dotenv import load_dotenv
 from decouple import config
 
@@ -23,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$=m+&aq5yim5#(a$re=olei8zx$@=e@&(4em!15f9t-cs$k6)p'
+
+
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -49,6 +52,7 @@ INSTALLED_APPS = [
     'marketing',
     'posts',
 
+    'cloudinary'
     'cloudinary_storage',
 ]
 

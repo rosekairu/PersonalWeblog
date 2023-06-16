f1ea64d948b79093e83e0009f8e7bac0cf8ce1a7
rosekairu <rosekairu@gmail.com>
2023-06-16 19:09:18 +0300

    modified settings

--- Blog/settings.py
+++ Blog/settings.py
@@ -137,14 +137,14 @@ load_dotenv()
 
 # Cloudinary Storage Access to the variables
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
 
-CLOUD_NAME = str(os.getenv('CLOUD_NAME'))
-API_KEY = str(os.getenv('API_KEY'))
-API_SECRET = str(os.getenv('API_SECRET'))
-
-
-CLOUDINARY_URL = str(os.getenv('CLOUDINARY_URL'))
-STATIC_URL = str(os.getenv('STATIC_URL'))
+CLOUDINARY_URL = config('CLOUDINARY_URL'),
+STATIC_URL = config('STATIC_URL')
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
